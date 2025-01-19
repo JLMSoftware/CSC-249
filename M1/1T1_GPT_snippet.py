50ef995143a244112770a746a0108aa3eb72c526
JLMSoftware <mitcheljprogramming@gmail.com>
2025-01-18 20:36:20 -0500

    1T1 code

---
+++ M1/1T1_GPT_snippet.py
@@ -0,0 +1,8 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Step 1: Create a list (linear data structure)
+... numbers = [1, 2, 3, 4, 5]
+... 
+... # Step 2: Iterate through the list and print each number
+... for number in numbers:
+...     print(number)
