099571130bacf5126842cdec631ee4f67c237c1e
JLMSoftware <mitcheljprogramming@gmail.com>
2025-02-23 15:57:03 -0500

    Update 2L1_LinearSearch_Mitchell.cpp

--- M2/2L1_LinearSearch/2L1_LinearSearch_Mitchell.cpp
+++ M2/2L1_LinearSearch/2L1_LinearSearch_Mitchell.cpp
@@ -1,62 +1,49 @@
 /*
-CSC-249
-2L1-Linear Search
+CSC 249
+2L2_Binary Search
 Justin Mitchell
-2/23/2025
+2/23/25
 */
 
 #include <iostream>
+
 using namespace std;
 
-int BinarySearch(float numbers[], int numbersSize, float key){
-    int mid = 0;
-    int low = 0;
-    int high = numbersSize - 1;
-
-    while (high >= low){
-        mid = (high + low) /2;
-        if (numbers[mid] < key) {
-            low = mid + 1;
-        } else if (numbers[mid] > key ){
-
-            high = mid - 1;
-        } else {
-
-            return mid;
+int LinearSearch(string numbers[], int numbersSize, string key){
+    for (int i = 0; i < numbersSize; i++){
+        if (numbers[i] == key){
+            return i;
         }
-
-
     }
-
     return -1;
+
 }
+
+
 
 
 int main()
 {
-    float numbers[8] = {0.25, 0.44, 0.7, 1.0, 1.24, 3.2, 4.5, 8.7};
-    int NUMBERS_SIZE = 8;
-    int i = 0;
-    float key = 0;
-    int keyIndex = 0;
+    string numbers[] = {"plane", "train", "brain", "mane", "cane", "octane"};
+    int numbersSize = sizeof(numbers) / sizeof(numbers[0]);
+    cout << "Words: [" << numbers[0];
 
-    cout << "Numbers: ";
-    for (i = 0; i < NUMBERS_SIZE; ++i){
-        cout << numbers[i] << " ";
+    for (int i =1; i < numbersSize; i++) {
+        cout << ", " << numbers[i];
     }
+    cout << "]" << endl;
 
-    cout << endl;
-
-    cout << "Enter a value: ";
+    cout << "Enter a string for key in ALL LOWERCASE!: ";
+    string key = "";
     cin >> key;
 
-    keyIndex = BinarySearch(numbers, NUMBERS_SIZE, key);
+    int keyIndex = LinearSearch(numbers, numbersSize, key);
 
-    if (keyIndex == -1){
-        cout << key << " was not found." << endl;
+    if (keyIndex == -1) {
+      cout << key << " was not found." << endl;
     } else {
-
-        cout << "Found " << key << " at index " << keyIndex << ".";
+      cout << "Found " << key << " at index ";
+      cout << keyIndex << "." << endl;
     }
 
 }
